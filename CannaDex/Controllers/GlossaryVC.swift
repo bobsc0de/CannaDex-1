4732fd1c0ccbe8d322ba700c1b5b9d3608131b16
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-07 16:00:04 +0500

    updated

---
+++ CannaDex/Controllers/GlossaryVC.swift
@@ -0,0 +1,29 @@
+//
+//  GlossaryVC.swift
+//  CannaDex
+//
+//  Created by Marwan Khalid on 3/7/22.
+//
+
+import UIKit
+
+class GlossaryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CannaDex/Views/GlossaryTVC.swift
@@ -0,0 +1,22 @@
+//
+//  GlossaryTVC.swift
+//  CannaDex
+//
+//  Created by Marwan Khalid on 3/7/22.
+//
+
+import UIKit
+
+class GlossaryTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+
+}
