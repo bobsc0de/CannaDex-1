88600d8308349157c2470af7943cad0cc7ccc31d
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-07 18:06:59 +0500

    updated

---
+++ CannaDex/Controllers/FavoriteVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteVC.swift
+//  CannaDex
+//
+//  Created by Marwan Khalid on 3/7/22.
+//
+
+import UIKit
+
+class FavoriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        
+    }
+
+}
