3e043fb4c7c5356ab59faf1cb2c1adfdf88e1e3e
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-07 16:16:03 +0500

    updated

---
+++ CannaDex/Controllers/FilterVC.swift
@@ -0,0 +1,18 @@
+//
+//  FilterVC.swift
+//  CannaDex
+//
+//  Created by Marwan Khalid on 3/7/22.
+//
+
+import UIKit
+
+class FilterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
