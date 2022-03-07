937bda9be9cfec0cd5c39c5a466753fd9867cb11
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-07 15:17:11 +0500

    updated

--- CannaDex/Controllers/Top10VC.swift
+++ CannaDex/Controllers/Top10VC.swift
@@ -31,61 +31,20 @@ extension Top10VC:UICollectionViewDelegate,UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeCVC.identifier, for: indexPath) as! HomeCVC
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCVC", for: indexPath) as! HomeCVC
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
-        if indexPath.row == 0 {
-            let controller = storyboard?.instantiateViewController(withIdentifier: GroupMessagesVC.identifier) as! GroupMessagesVC
-            controller.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(controller, animated: true)
-        }
-        
-//        if indexPath.row == 0 {
-//            let controller = storyboard?.instantiateViewController(withIdentifier: GroupMembersVC.identifier) as! GroupMembersVC
-//            controller.modalPresentationStyle = .fullScreen
-//            self.navigationController?.pushViewController(controller, animated: true)
-//        }
-        
-        if indexPath.row == 1 {
-            let controller = storyboard?.instantiateViewController(withIdentifier: MessagesListVC.identifier) as! MessagesListVC
-            controller.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(controller, animated: true)
-        }
-        
-        if indexPath.row == 2 {
-            let controller = storyboard?.instantiateViewController(withIdentifier: AvailableShiftsVC.identifier) as! AvailableShiftsVC
-            controller.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(controller, animated: true)
-        }
-        
-        if indexPath.row == 3 {
-            let controller = storyboard?.instantiateViewController(withIdentifier: ScheduleVC.identifier) as! ScheduleVC
-            controller.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(controller, animated: true)
-        }
-        
-        if indexPath.row == 4 {
-            let controller = storyboard?.instantiateViewController(withIdentifier: ClockVC.identifier) as! ClockVC
-            controller.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(controller, animated: true)
-        }
-        
-        if indexPath.row == 5 {
-            let controller = storyboard?.instantiateViewController(withIdentifier: DocumentVC.identifier) as! DocumentVC
-            controller.modalPresentationStyle = .fullScreen
-            self.navigationController?.pushViewController(controller, animated: true)
-        }
         
         
     }
     
 }
 
-extension HomeVC:UICollectionViewDelegateFlowLayout{
+extension Top10VC:UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let collectionwidth = collectionView.bounds.width
         return CGSize(width: collectionwidth/2 - 30, height: 150)
