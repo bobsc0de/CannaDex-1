ac89e0c9cb6bc92c5e014c9a4e1a37837e517bcc
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-08 15:04:12 +0500

    updated

---
+++ CannaDex/Controllers/DetailGlossaryVC.swift
@@ -0,0 +1,18 @@
+//
+//  DetailGlossaryVC.swift
+//  CannaDex
+//
+//  Created by Marwan Khalid on 3/8/22.
+//
+
+import UIKit
+
+class DetailGlossaryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
