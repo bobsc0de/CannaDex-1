05441fdf116e88f1c6d8d6759d4cac05322be541
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-08 16:08:16 +0500

    updated

--- CannaDex/Controllers/DetailGlossaryVC.swift
+++ CannaDex/Controllers/DetailGlossaryVC.swift
@@ -26,6 +26,9 @@ class DetailGlossaryVC: UIViewController {
         setupDesign()
         showView(button: "de")
         textT.text = "Descri"
+        descriptionB.backgroundColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
+        referencesB.backgroundColor = .clear
+        dateB.backgroundColor = .clear
         descriptionB.titleLabel?.textColor = UIColor.white
     }
     
@@ -37,24 +40,25 @@ class DetailGlossaryVC: UIViewController {
     private func showView(button:String) {
         if button == "de"{
             descriptionB.layer.cornerRadius = 12.0
-            descriptionB.backgroundColor = .green
-            descriptionB.titleLabel?.textColor = UIColor.white
+            descriptionB.backgroundColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
+            descriptionB.setTitleColor(.white, for: .normal)
             referencesB.backgroundColor = .clear
             referencesB.titleLabel?.textColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
             dateB.backgroundColor = .clear
             dateB.titleLabel?.textColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
         }else if button == "re" {
             referencesB.layer.cornerRadius = 12.0
-            referencesB.backgroundColor = .green
-            referencesB.titleLabel?.textColor = UIColor.white
+            referencesB.backgroundColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
+            referencesB.setTitleColor(.white, for: .normal)
             descriptionB.backgroundColor = .clear
             descriptionB.titleLabel?.textColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
             dateB.backgroundColor = .clear
             dateB.titleLabel?.textColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
         }else if button == "da" {
             dateB.layer.cornerRadius = 12.0
-            dateB.backgroundColor = .green
-            dateB.titleLabel?.textColor = UIColor.white
+            dateB.backgroundColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
+            //dateB.tintColor = UIColor.white
+            dateB.setTitleColor(.white, for: .normal)
             descriptionB.backgroundColor = .clear
             descriptionB.titleLabel?.textColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
             referencesB.titleLabel?.textColor = #colorLiteral(red: 0.1961533725, green: 0.7796291709, blue: 0.3441372514, alpha: 1)
@@ -69,15 +73,20 @@ class DetailGlossaryVC: UIViewController {
     @IBAction func descriptionB(_ sender: Any) {
         showView(button: "de")
         textT.text = "Descri"
+        descriptionB.setTitleColor(UIColor.white, for: .normal)
+        descriptionB.tintColor = .white
     }
     
     @IBAction func referencesB(_ sender: Any) {
         showView(button: "re")
         textT.text = "REfere"
+        referencesB.setTitleColor(UIColor.white, for: .normal)
+        referencesB.tintColor = .white
     }
     
     @IBAction func dateB(_ sender: Any) {
         showView(button: "da")
         textT.text = "Date"
+        dateB.setTitleColor(UIColor.white, for: .normal)
     }
 }
