52f55873701fbe4409f5d4c92458d9ba388c1ecf
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-08 15:08:18 +0500

    updatedupdated

--- CannaDex/Controllers/DetailGlossaryVC.swift
+++ CannaDex/Controllers/DetailGlossaryVC.swift
@@ -9,10 +9,35 @@ import UIKit
 
 class DetailGlossaryVC: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var nameL: UILabel!
+    @IBOutlet weak var categL: UILabel!
+    @IBOutlet weak var favoriteB: UIButton!
+    @IBOutlet weak var descriptionB: UIButton!
+    @IBOutlet weak var dateB: UIButton!
+    @IBOutlet weak var referencesB: UIButton!
+    @IBOutlet weak var textT: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
     }
 
+    @IBAction func favoriteB(_ sender: Any) {
+        
+    }
+    
+    @IBAction func descriptionB(_ sender: Any) {
+        
+    }
+    
+    @IBAction func referencesB(_ sender: Any) {
+        
+    }
+    
+    @IBAction func dateB(_ sender: Any) {
+        
+    }
 }
