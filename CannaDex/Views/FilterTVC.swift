80b82bed03e262e83591b7fd05e349885fa1fa77
Marwan Khalid <marwankhalid44@gmail.com>
2022-03-08 23:58:30 +0500

    updated

--- CannaDex/Views/FilterTVC.swift
+++ CannaDex/Views/FilterTVC.swift
@@ -12,19 +12,22 @@ class FilterTVC: UITableViewCell {
     @IBOutlet weak var filterName: UILabel!
     @IBOutlet weak var checkBoxB: UIButton!
 
+    var check : (() -> Void)? = nil
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
     }
     
     @IBAction func checkBoxB(_ sender: Any) {
-        
+        if let btnAction = self.check {
+                    btnAction()
+        }
     }
 
 }
